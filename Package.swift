46a1416e17e714a30ace3ffa9c9cf871ffedf622
Mohanraj1417 <mohanraj@datasense.in>
2025-11-13 14:17:43 +0530

    Commit for support version update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MGDropdown",
+    platforms: [
+            .iOS(.v15)   // 👈 REQUIRED for SwiftUI
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
